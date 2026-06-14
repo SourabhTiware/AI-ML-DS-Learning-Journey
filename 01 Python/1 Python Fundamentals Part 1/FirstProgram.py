bd593a1449d6f2b3cb19b4f498ee753ad39b209f
Sourabh Tiware <sourabhtiware1737@gmail.com>
2026-06-14 22:44:10 +0530

    Restart AI journy Day 1

--- 01 Python/1 Python Fundamentals Part 1/FirstProgram.py
+++ 01 Python/1 Python Fundamentals Part 1/FirstProgram.py
@@ -1,3 +1,214 @@
-print("Hello World")
 
-x = 76
+# --------------------------------------------------------------------------
+# Lecture 1: Our First Program
+# --------------------------------------------------------------------------
+
+
+# print("Lecture 1: Our First Program")
+# print("Hello World")
+
+# x = 76
+
+# print(x + 40)
+
+
+# --------------------------------------------------------------------------
+# Lecture 2: Variables in Python
+# --------------------------------------------------------------------------- 
+
+
+# print("Lecture 2: Variables in python")
+# name = "Sourabh Tiware"
+
+# _age = 25
+
+# city_name = "pune"
+
+# print(name)
+# print(_age)
+# print(city_name)
+
+# print("my name is", name, "and i'm",_age,"year's old." "\ni'm belongs from",city_name,"city")
+
+# --------------------------------------------------------------------------
+# Lecture 3: Data Types in Python
+# --------------------------------------------------------------------------- 
+# print("Lecture 3: Data Types in Python")
+
+# print("Integer Types ")
+
+# age = 34
+# bank_balance = 45990
+
+# print("age",age,"and total amount ", bank_balance)
+
+# print("check type")
+
+# print(type(age))
+# print(type(bank_balance))
+
+# print("String")
+
+# name = "Hello, I'm Sourabh Tiware"
+# qualificatiion = "MCA"
+
+# print(name,qualificatiion)
+
+# print("check type")
+
+# print(type(name))
+# print(type(qualificatiion))
+
+# print("float")
+
+# pi = 3.14
+# x = 8.4
+
+# print(pi,"and",x)
+
+# print("check type")
+
+# print(type(pi))
+# print(type(x))
+
+
+# print("Boolean")
+
+# isPrime = True
+# nonPrime = None
+
+# print(isPrime)
+# print(nonPrime)
+
+# print("check type")
+
+# print(type(isPrime))
+# print(type(nonPrime))
+
+# print("None type")
+
+# car_value = None
+# print(car_value)
+
+# print("check type")
+
+# print(type(car_value))
+
+
+# --------------------------------------------------------------------------
+# Lecture 4: Keywords & comments in Python
+# --------------------------------------------------------------------------- 
+# print("Lecture 4: Keywords & comments in Python")
+
+#  Keywords - those words are not used for declaring variable name / function name. 
+
+#  Single Line comments = #
+
+''' this is a multiple comments. '''
+
+
+# --------------------------------------------------------------------------
+# Lecture 5: style Guide in Python
+# --------------------------------------------------------------------------- 
+# print("Lecture 5: style Guide in Python")
+
+
+# print("Snake case = total_price")
+# print("camcel case = totalPrice")
+# print("Pascal Casse = TotalPrice")
+
+# print("Python prefer SNAKE CASE ")
+
+# # Task - calculate sum of two number 
+
+# print("\n \nTask - calculate sum of two number ")
+
+# a = 5
+# b = 10
+
+# sum = a + b
+# print("sum of two numbers ",sum)
+
+
+# --------------------------------------------------------------------------
+# Lecture 6: Arithmetic operator in Python
+# --------------------------------------------------------------------------- 
+# print("Lecture 6: Arithmetic operator in Python\n\n")
+
+# a = 5
+# b = 10
+
+# print("a = 5 & b = 10 \n")
+
+# print("addition oprator a + b:", a + b,"\n")
+# print("substraction oprator a - b:", a - b,"\n")
+# print("multiplication oprator a * b:", a * b,"\n")
+# print("division oprator a / b:", a / b,"\n")
+# print("module oprator a '%' b:", a % b,"\n") # calculate reminder
+# print("power oprator a ** b:", a ** b,"\n")
+
+
+# --------------------------------------------------------------------------
+# Lecture 7: Relational / comparison operator in Python
+# --------------------------------------------------------------------------- 
+# print("Lecture 7: Relational / comparison operator in Python\n\n")
+
+
+# a = 10
+# b = 5
+
+# print("a = 10 & b = 5 \n")
+
+# print("greate than: ", a > b,"\n")
+# print("greate than equal to : ", a >= b,"\n")
+# print("less than: ", a < b,"\n")
+# print("less than equal to : ", a <= b,"\n")
+# print("equal to : ", a == b,"\n")
+# print("not equl to : ", a != b,"\n")
+
+
+# --------------------------------------------------------------------------
+# Lecture 8: Assignment operator in Python
+# --------------------------------------------------------------------------- 
+print("Lecture 8: Assignment operator in Python\n\n")
+
+
+a = 10
+b = a
+
+
+
+print("equal to ", b)
+
+a += 5
+print("plus equal to a += 5  : ",a)
+
+d = 10
+
+d -= 3
+print("minus equal to d = 10, d -= 3 : ",d)
+
+
+x = 15 
+
+x *=3
+print("multiple equal to x = 15, x*=3 : ", x)
+
+
+z = 10
+
+z /=2
+
+print("divide equal to z = 10, z /= 2 : ", z)
+
+b = 4
+
+b **=2
+
+print("power equal to b = 4, b **=2 : ", b)
+
+r = 10
+
+r %=2
+
+print("module equal to r = 10, r %=2 : ", r)
