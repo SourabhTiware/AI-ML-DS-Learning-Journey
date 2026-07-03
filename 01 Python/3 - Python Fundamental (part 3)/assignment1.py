c6ccc7cedb3fc9a8c8cc68b4f135de5c70a50ca6
Sourabh Tiware <sourabhtiware1737@gmail.com>
2026-07-03 22:34:18 +0530

    Day 17: AI-ML-DS

--- 01 Python/3 - Python Fundamental (part 3)/assignment1.py
+++ 01 Python/3 - Python Fundamental (part 3)/assignment1.py
@@ -349,3 +349,16 @@ while(True):
             print("\nWrong input ...")
 
 
+# Q6 - Given a list of words = ["apple", "banana", "kiwi", "cherry", "mangor"]
+# create a dictionary that maps each word to its length. 
+# Example : {"apple":5, "banana" : 6, "kiwi": 4, ...}
+
+words = ["apple", "banana", "kiwi", "cherry", "mangor"]
+new_words = {}
+
+for i in range(len(words)):
+    new_words.update({
+        words[i] : len(words[i])
+    })
+
+print(new_words)
