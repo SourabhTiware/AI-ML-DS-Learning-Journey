a6c847b82a844ece7029072edbb97e72bc4249db
Sourabh Tiware <sourabhtiware1737@gmail.com>
2026-07-06 08:55:21 +0530

    Day 19 & 20: AI-ML-DS

--- 01 Python/4 - Python Fundamental (Part 4)/py_fundamentals.py
+++ 01 Python/4 - Python Fundamental (Part 4)/py_fundamentals.py
@@ -358,30 +358,176 @@
 
 #                                       Lec -15 - Polymorphism ( Duck Typing)
 
-class Dog:
-    def speak(self):
-        print("Dog")
+# class Dog:
+#     def speak(self):
+#         print("Dog")
 
-class Cat:
-    def speak(self):
-        print("Cat")
+# class Cat:
+#     def speak(self):
+#         print("Cat")
 
-class Robot:
-    def speak(self):
-        print("Robot")
+# class Robot:
+#     def speak(self):
+#         print("Robot")
 
-def make_it_speak(entity):
-    entity.speak()          # Doesn't care about type
+# def make_it_speak(entity):
+#     entity.speak()          # Doesn't care about type
 
 
-d = Dog()
-# d.speak()
+# d = Dog()
+# # d.speak()
 
-c = Cat()
-# c.speak()
+# c = Cat()
+# # c.speak()
 
-r = Robot()
-# r.speak()
+# r = Robot()
+# # r.speak()
 
-for i in (d,c,r):
-    make_it_speak(i)
+# for i in (d,c,r):
+#     make_it_speak(i)
+
+
+# Python Journey Restart 
+
+# -----------------------------------------------------------------------------
+# Lec 01: What is object oriented programming
+# Lec 02: Classess & Objects
+# -----------------------------------------------------------------------------
+
+# class student:
+#     subject = "python"
+#     college = "SnapSoft technology"
+#     year = 2026
+
+
+
+# stu1 = student()
+# print(stu1)
+# print(type(stu1))
+
+
+# -----------------------------------------------------------------------------
+# Lec 03: Attributes and Methods
+# Lec 04: Constructor - init() method
+# -----------------------------------------------------------------------------
+
+
+# class student:
+#     def __init__(self):
+#         print("Constructor was called")
+
+# # constructor was called by python on creation time of the object. like stu1 and stu2.
+
+# stu1 = student()
+# stu2 = student()
+
+# print(f"type of stu1 is {type(stu1)} and it's value stored in {stu1}")
+
+
+# class student:
+#     def __init__(self, name, cgpa):
+#         self.name = name
+#         self.cgpa = cgpa
+    
+# stu1 = student("sourabh",9.3)
+# stu2 = student("komal", 9.8)
+# stu3 = student("preesh", 9.9)
+
+# print(f"I'm {stu1.name}, and I got cgpa {stu1.cgpa}")
+# print(f"I'm {stu2.name}, and I got cgpa {stu2.cgpa}")
+# print(f"I'm {stu3.name}, and I got cgpa {stu3.cgpa}")
+
+
+
+# -----------------------------------------------------------------------------
+# Lec 05: Types of Constructors
+# -----------------------------------------------------------------------------
+
+# class stuendet:
+#     def __init__(self,name,cgpa): # parameterized constructor
+#         self.name = name
+#         self.cgpa = cgpa
+    
+#     def get_cgpa(self):
+#         return self.cgpa
+    
+
+# stu1 = stuendet("Rahul", 9.0)
+# stu2 = stuendet("Urvashi", 8.4)
+# stu3 = stuendet("sourabh",9.9)
+
+# print(f"{stu1.name} has cgpa = {stu1.get_cgpa()}")
+
+
+
+# -----------------------------------------------------------------------------
+# Lec 06: Attributes - class & Instance
+# -----------------------------------------------------------------------------
+
+# class employee:
+#     orginazation_name = "SnapSoft Technology"
+#     location = "pune"
+
+#     def __init__(self, emp_name, location):
+#         self.emp_name = emp_name
+#         self.location = location
+
+# emp_1 = employee("sourabh","mumbai")
+
+# print("instance attribute : ",emp_1.location)
+# print("class attribute : ",employee.location)
+
+# print("also using object I can access class attribute : ",emp_1.orginazation_name)
+
+
+# -----------------------------------------------------------------------------
+# Lec 07: Intance method
+# -----------------------------------------------------------------------------
+
+# class laptop:
+#     storage_type = "ssd"
+
+#     def __init__(self,RAM, storage):
+#         self.RAM = RAM
+#         self.storage = storage
+
+#     def get_info(self): # Intance method - it can access intance attribute + class attributes
+#         print(f"laptop has {self.RAM} RAM and {self.storage} {self.storage_type}")
+
+
+# l1 = laptop("16gb", "512gb")
+# l2 = laptop("8gb", "256gb")
+
+# l1.get_info()
+
+# -----------------------------------------------------------------------------
+# Lec 08: class method
+# -----------------------------------------------------------------------------
+
+class laptop:
+    storage_type = "ssd"
+
+    def __init__(self,RAM, storage):
+        self.RAM = RAM
+        self.storage = storage
+    
+    @classmethod
+    def get_storage_type(cls):
+        print(f"storage type = {cls.storage_type}")
+    
+    @staticmethod
+    def cal_discount(price, discout):
+        final_price = price - (discout * price/ 100)
+        print(final_price)
+
+l1 = laptop("16gb", "512gb")
+
+l1.get_storage_type()
+
+laptop.get_storage_type()
+
+l1.cal_discount(40_000,10)
+
+
+
+
